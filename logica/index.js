4201c1c4cf4ced96851152454cf10d1207e63df6
JosePajoc <computacion.programacion.web@gmail.com>
2026-06-03 13:58:03 -0600

    nuevo dominio

--- logica/index.js
+++ logica/index.js
@@ -2,7 +2,8 @@ const estadoServer = document.getElementById("estadoServer");
 
 async function conectarServidor(){
     try{
-        const respuesta = await fetch("http://127.0.0.1:8000");
+        //const respuesta = await fetch("http://127.0.0.1:8000");
+        const respuesta = await fetch("https://control-usuario-backend-inicio.onrender.com");
         if(respuesta.ok){
             estadoServer.classList.remove("errorTexto");
             const datos = await respuesta.json()
